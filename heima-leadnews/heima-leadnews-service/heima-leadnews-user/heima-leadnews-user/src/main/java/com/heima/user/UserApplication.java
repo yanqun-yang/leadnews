9638315e2fbe2b0e66871a6fc056fc84e41a2cab
YanqunYang <2095291113@qq.com>
2024-09-25 00:58:27 +0800

    初始工程与登录接口的实现

---
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-user/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.heima.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient //集成当前的注册中心
+@MapperScan("com.heima.user.mapper")
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
