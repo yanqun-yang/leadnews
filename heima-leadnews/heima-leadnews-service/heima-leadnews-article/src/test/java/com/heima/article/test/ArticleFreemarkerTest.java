17d89e0606c6d1ff8fa7efcca7c4446450f7ad68
YanqunYang <2095291113@qq.com>
2024-09-29 20:02:04 +0800

    完成了文章列表查询、通过freemarker生成静态页面上传到minio

---
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-article/src/test/java/com/heima/article/test/ArticleFreemarkerTest.java
@@ -0,0 +1,66 @@
+package com.heima.article.test;
+
+import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.heima.article.ArticleApplication;
+import com.heima.article.mapper.ApArticleContentMapper;
+import com.heima.article.service.ApArticleService;
+import com.heima.file.service.FileStorageService;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleContent;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(classes = ArticleApplication.class)
+@RunWith(SpringRunner.class)
+public class ArticleFreemarkerTest {
+
+    @Autowired
+    private ApArticleContentMapper apArticleContentMapper;
+
+    @Autowired
+    private Configuration configuration;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Autowired
+    private ApArticleService apArticleService;
+
+    @Test
+    public void createStaticUrlTest() throws Exception {
+        //已知文章的id
+        //1.获取文章内容
+        ApArticleContent apArticleContent = apArticleContentMapper.selectOne(Wrappers.<ApArticleContent>lambdaQuery().eq(ApArticleContent::getArticleId, 1383827787629252610L));
+        if (apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())) {
+            //2.文章内容通过freemarker生成html文件
+            Template template = configuration.getTemplate("article.ftl");
+            //数据模型
+            Map<String, Object> content = new HashMap<>();
+            content.put("content", JSONArray.parseArray(apArticleContent.getContent()));
+            StringWriter out = new StringWriter();
+            //合成
+            template.process(content, out);
+            //3.把html文件上传到minio中
+            InputStream in = new ByteArrayInputStream(out.toString().getBytes());
+            String path = fileStorageService.uploadHtmlFile("", apArticleContent.getArticleId() + ".html", in);
+
+            //4.修改ap_article表,保存static_url字段
+            apArticleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId, apArticleContent.getArticleId())
+                    .set(ApArticle::getStaticUrl, path));
+        }
+    }
+}
+
